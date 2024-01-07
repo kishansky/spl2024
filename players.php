281b75910eceb9800906205015564fdaf7d67b32
Kishan Kumar <110089579+kishansky@users.noreply.github.com>
2024-01-07 10:35:05 +0530

    Update players.php

--- players.php
+++ players.php
@@ -38,7 +38,7 @@ if (!isset($_SESSION['admin'])) {
                     <tr
                         class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            <?php echo ucwords($row['name']); ?>
+                            <a href="player-details.php?id=<?php echo $row['p_id']; ?>"><?php echo ucwords($row['name']); ?></a>
                         </th>
                         <td class="px-6 py-4">
                             <?php echo $row['email']; ?>
